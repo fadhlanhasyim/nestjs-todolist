e9719478f1f3b70f88dea2914a6fecb724cdd9db
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2022-11-28 15:57:11 +0700

    resolve error

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { Todo } from './todo.entitiy';
-import { TypeOrmModule } from '@nestjs/typeorm';
+// import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
 import { PrismaClient } from '@prisma/client';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Todo])
+        // TypeOrmModule.forFeature([Todo])
     ],
     controllers: [TodoController],
     providers: [TodoService, PrismaClient],
