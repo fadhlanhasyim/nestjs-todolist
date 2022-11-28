f0ba102023223d27cef09756313d98f4b6c79fb5
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2022-11-28 15:46:55 +0700

    Delete typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,18 +6,6 @@ import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '123',
-      database: 'todo',
-      synchronize: true,
-      dropSchema: false,
-      logging: true,
-      entities: ['src/**/*.entitiy.js', 'dist/**/*.entitiy.js'],
-    }),
     TodoModule,
   ],
   controllers: [AppController],
